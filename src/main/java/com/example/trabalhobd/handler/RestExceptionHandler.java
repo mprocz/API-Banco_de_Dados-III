fa8ec35bb5191f0651d8ab60ac787c7eac83a209
mprocz <marcosprocykevicz@gmail.com>
2024-06-10 11:43:28 -0300

    exceções

---
+++ src/main/java/com/example/trabalhobd/handler/RestExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.example.trabalhobd.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.trabalhobd.common.ConversorData;
+import com.example.trabalhobd.domain.exception.BadRequestException;
+import com.example.trabalhobd.domain.exception.ResourceNotFoundException;
+import com.example.trabalhobd.domain.model.ErroResposta;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErroResposta> handlerResourceNotFoundException(ResourceNotFoundException ex){
+        String dataHora = ConversorData.converterDateParaDataHora(new Date());
+        ErroResposta erro = new ErroResposta(dataHora, HttpStatus.NOT_FOUND.value(), "NOT FOUND", ex.getMessage());
+        return new ResponseEntity<>(erro, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErroResposta> handlerBadException(ResourceNotFoundException ex){
+        String dataHora = ConversorData.converterDateParaDataHora(new Date());
+        ErroResposta erro = new ErroResposta(dataHora, HttpStatus.BAD_REQUEST.value(), "Bad Request", ex.getMessage());
+        return new ResponseEntity<>(erro, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErroResposta> handler(ResourceNotFoundException ex){
+        String dataHora = ConversorData.converterDateParaDataHora(new Date());
+        ErroResposta erro = new ErroResposta(dataHora, HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error", ex.getMessage());
+        return new ResponseEntity<>(erro, HttpStatus.NOT_FOUND);
+    }
+}
